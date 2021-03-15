8176a7b03b7b5ec0beca3df30056106a43605515
kakashiZy <thiagoandradesx@gmail.com>
2021-03-15 13:35:45 -0300

    First commit

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/5598987851993 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO KAKASHI 🐊🚩 :*
+_https://chat.whatsapp.com/Jk8wX5Mj8aLLs8rNfhrNxM`
+}
+exports.daftarvip = daftarvip
